e0bbea5d741b43b79eb93580dcc4384f5e3ff4c8
widadsutejo <114811459+widadsutejo@users.noreply.github.com>
2024-05-24 13:56:16 +0700

    Update registrasi.php

--- destinasijatim/registrasi.php
+++ destinasijatim/registrasi.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$sesiData = !empty($_SESSION['sesiData']) ? $_SESSION['sesiData'] : '';
-if (!empty($sesiData['status']['msg'])) {
+$sesiData = !empty($_SESSION['sesiData'])?$_SESSION['sesiData']:'';
+if(!empty($sesiData['status']['msg'])){
     $statusPsn = $sesiData['status']['msg'];
     $jenisStatusPsn = $sesiData['status']['type'];
     unset($_SESSION['sesiData']['status']);
@@ -9,96 +9,137 @@ if (!empty($sesiData['status']['msg'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Registrasi - DESTINASI JATIM</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Registrasi - JOGJAKU</title>
+
+    <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
     <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link rel="shortcut icon" href="images/destinasijatim.jpg">
+    <link rel="shortcut icon" href="images/jogjakublack.png">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
+
 <body>
+
+    <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <a href="index.php"><img src="images/destinasijatim.jpg" style="width:130px; height: 50px"></a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a  href="index.php"><img src="images/jogjaku.png" style="width:130px; height: 50px"></a>
             </div>
+             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <?php if (!isset($_SESSION['is_login'])) { ?>
-                    <li class="active"><a href="index.php">Home</a></li>
-                    <?php } else { ?>
-                    <li><a href="akunuser.php?logoutSubmit=1" class="logout">Logout</a></li>
-                    <?php } ?>
+                    <li class="active">
+                        <a href="login.php">Login</a>
+                    </li>
                 </ul>
             </div>
+            <!-- /.navbar-collapse -->
         </div>
+        <!-- /.container -->
     </nav>
-    <br><br><br><br><br>
-    <div class="container" style="margin-top: 30px;">
-        <div class="col-sm-4 col-sm-offset-4">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <h3><center>Registrasi Akun</h3><br>
-                    <?php echo !empty($statusPsn) ? '<p class="' . $jenisStatusPsn . '">' . $statusPsn . '</p>' : ''; ?>
-                    <div class="regisForm">
-                        <form id="register-form" action="registrasi_proses.php" method="post" role="form">
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <input type="text" name="nama_awal" placeholder="Nama Awal" required class="form-control" />
-                            </div>
-                            <br>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <input type="text" name="nama_akhir" placeholder="Nama Akhir" required class="form-control" />
-                            </div>
-                            <br>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                                <input type="email" name="email" placeholder="Email" required class="form-control" />
-                            </div>
-                            <br>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                <input type="password" name="password" placeholder="Password" required class="form-control" />
-                            </div>
-                            <br>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-phone"></i></span>
-                                <input type="text" name="telp" placeholder="No Telepon" required class="form-control" />
-                            </div>
-                            <br>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <select name="role" class="form-control">
-                                    <option value="user_biasa">User</option>
-                                    <option value="admin">Admin</option>
-                                </select>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <input type="submit" name="registerSubmit" value="Daftar" class="btn btn-primary btn-block">
-                            </div>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        </br>
+        </br>
+        </br>
+        
+        <div class="container, justify" style="margin-top: 30px;">
+        <?php echo !empty($statusPsn)?'<p class="'.$jenisStatusPsn.'">'.$statusPsn.'</p>':''; ?>
+            <div class="col-md-6 col-sm-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <form id="login-form" method="post" action="akunuser.php" role="form">
+                        <legend>Registrasi</legend>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input type="text" name="nama_awal" placeholder="Nama awal" required class="form-control" />
+                        </div>
+                        </br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input type="text" name="nama_akhir" placeholder="Nama akhir" required class="form-control" />
+                        </div>
+                        </br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+                            <input type="text" name="email" placeholder="Alamat email" required class="form-control" />
+                        </div>
+                        </br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-phone"></i></span>
+                            <input type="text" name="telp" placeholder="Nomor telepon" required class="form-control" />
+                        </div>
+                        </br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input type="password" name="password" placeholder="Password" required class="form-control" />
+                        </div>
+                        </br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input type="password" name="confirm_password" placeholder="Konfirmasi Password" required class="form-control" />
+                        </div>
+                        </br>
+                        <div class="form-group">
+                            <input type="submit" name="daftarSubmit" value="Daftar" class="btn btn-primary btn-block" />
+                        </div>
                         </form>
-                    </div>
-                    <div class="form-group">
-                        <div class="center" style="padding: 0;">Sudah punya akun? <a href="login.php">Login disini</a></div>
-                        <div class="col-sm-6 text-right brand" style="padding: 0;"></div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <footer>
-        <div class="row">
-            <div class="col-lg-12">
-                <br><br><br>
-                <p align="center">Copyright © 2024 destinasijatim.com All Rights Reserved. Made by yani</p>
-            </div>
-        </div>
-    </footer>
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
 
+
+
+        <!-- Footer -->
+        <footer>
+        <div class="row">
+                <div class="col-lg-12">
+                    <p align="center">Copyright © 2015 jogjaku.com All Rights Reserved. Made by Vitto | Distributed By <a href="https://blogbugabagi.blogspot.com" target="_blank" rel="noopener noreferrer">BlogBugaBagi</a></p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
