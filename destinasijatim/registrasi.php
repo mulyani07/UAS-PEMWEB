93aa6ad0c1446eb58f6ffeaca6fd219e6df5be28
widadsutejo <114811459+widadsutejo@users.noreply.github.com>
2024-05-27 00:49:04 +0700

    Update registrasi.php

--- destinasijatim/registrasi.php
+++ destinasijatim/registrasi.php
@@ -9,9 +9,7 @@ if(!empty($sesiData['status']['msg'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,11 +20,7 @@ if(!empty($sesiData['status']['msg'])){
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
     <link href="css/modern-business.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link rel="shortcut icon" href="images/jogjakublack.png">
 
@@ -36,15 +30,11 @@ if(!empty($sesiData['status']['msg'])){
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
-
 <body>
-
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -52,9 +42,8 @@ if(!empty($sesiData['status']['msg'])){
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a  href="index.php"><img src="images/jogjaku.png" style="width:130px; height: 50px"></a>
+                <a href="index.php"><img src="images/jogjaku.png" style="width:130px; height: 50px"></a>
             </div>
-             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="active">
@@ -62,18 +51,12 @@ if(!empty($sesiData['status']['msg'])){
                     </li>
                 </ul>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
     </nav>
 
     <!-- Page Content -->
     <div class="container">
-
-        </br>
-        </br>
-        </br>
-        
+        </br></br></br>
         <div class="container, justify" style="margin-top: 30px;">
         <?php echo !empty($statusPsn)?'<p class="'.$jenisStatusPsn.'">'.$statusPsn.'</p>':''; ?>
             <div class="col-md-6 col-sm-offset-3">
@@ -112,6 +95,14 @@ if(!empty($sesiData['status']['msg'])){
                         </div>
                         </br>
                         <div class="form-group">
+                            <label for="role">Pilih Peran:</label>
+                            <select name="role" class="form-control" required>
+                                <option value="user_biasa">User Biasa</option>
+                                <option value="admin">Admin</option>
+                            </select>
+                        </div>
+                        </br>
+                        <div class="form-group">
                             <input type="submit" name="daftarSubmit" value="Daftar" class="btn btn-primary btn-block" />
                         </div>
                         </form>
@@ -119,8 +110,6 @@ if(!empty($sesiData['status']['msg'])){
                 </div>
             </div>
         </div>
-
-
 
         <!-- Footer -->
         <footer>
@@ -130,16 +119,12 @@ if(!empty($sesiData['status']['msg'])){
                 </div>
             </div>
         </footer>
-
     </div>
     <!-- /.container -->
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
 </body>
-
 </html>
