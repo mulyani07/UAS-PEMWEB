45fd1e36cd088ce5d3476eca8e035919e78693d3
Numenor08 <athaya120@gmail.com>
2024-06-14 14:43:02 +0700

    fix some minor design and delete search button

--- index.php
+++ index.php
@@ -166,18 +166,6 @@ $events = $req->fetchAll();
             <!-- Blog Sidebar Widgets Column -->
             <div class="col-md-4">
 
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Pencarian</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
-                </div>
-
                 <!-- Blog Categories Well -->
                 <div class="well">
                     <h4>Kategori</h4>
